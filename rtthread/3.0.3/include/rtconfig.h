03ad407deee4696bd9438a704749271bdb68431e
xiaobing <xiaobing_yin@163.com>
2020-10-17 10:16:01 +0800

    完成对象容器的实现功能

--- rtthread/3.0.3/include/rtconfig.h
+++ rtthread/3.0.3/include/rtconfig.h
@@ -2,6 +2,8 @@
 #define __RTTHREAD_CFG_H__
 
 #define RT_THREAD_PRIORITY_MAX			32			/*最大优先级*/
-#define RT_ALIGN_SIZE					4			/*多少个字节对齐*/
+#define RT_ALIGN_SIZE								4				/*多少个字节对齐*/
+
+#define RT_NAME_MAX									8				/*内核对象的名字长度*/
 
 #endif
